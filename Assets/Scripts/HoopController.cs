53f0942335604f55eb04a5d9fbe3635fc99942ce
Mete Yıldırım <43517475+meteyldrm@users.noreply.github.com>
2021-12-04 06:24:17 +0300

    Import TMP and add the "Baloo2" font

---
+++ Assets/Scripts/HoopController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class HoopController: MonoBehaviour {
+    public int totalScore = 0;
+    public int totalBaskets = 0;
+
+    [SerializeField]
+    private TextMeshProUGUI BasketsTMP;
+        
+    [SerializeField]
+    private TextMeshProUGUI ScoreTMP;
+
+    private void Start() {
+        totalScore = 0;
+        totalBaskets = 0;
+        updateUI();
+    }
+
+    public void onBasket(int score) {
+        totalScore += score;
+        totalBaskets += 1;
+        updateUI();
+    }
+
+    private void updateUI() {
+        BasketsTMP.text = $"Baskets Scored: {totalBaskets}";
+        ScoreTMP.text = $"Total Score: {totalScore}";
+    }
+}
