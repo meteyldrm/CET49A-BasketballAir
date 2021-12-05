e5eb1c3191f65d57a8cd0c2373f4d9ab64ce7318
Mete Yıldırım <43517475+meteyldrm@users.noreply.github.com>
2021-12-06 01:16:35 +0300

    Add drag offset to basketballs and scene boundaries.

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -7,6 +7,7 @@ public class Draggable : MonoBehaviour {
     
     public bool canDrag = true;
     private bool dragging;
+    private Vector3 dragOffset = Vector3.zero;
 
     private Camera cam;
 
@@ -21,6 +22,7 @@ public class Draggable : MonoBehaviour {
 
     private void OnDisable() {
         dragging = false;
+        dragOffset = Vector3.zero;
     }
 
     private void FixedUpdate() {
@@ -34,12 +36,15 @@ public class Draggable : MonoBehaviour {
 
             if (touch.phase == TouchPhase.Began) {
                 var ray = cam.ScreenPointToRay(touch.position);
-                Physics.Raycast(ray, out var hit, 100);
-                
+                RaycastHit hit;
+                Physics.Raycast(ray, out hit, 100);
+
                 //TODO Add a child to every basketball and increase its collider size to match finger size.
                 
                 try {
                     if (canDrag && hit.collider.gameObject == this.gameObject) {
+                        dragOffset = cam.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, -cam.transform.position.z)) - transform.position;
+                        dragOffset.z = 0;
                         dragging = true;
                         doDrag(true);
                     }
@@ -50,7 +55,7 @@ public class Draggable : MonoBehaviour {
 
             if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) {
                 if (canDrag && dragging) {
-                    Vector3 screenVector = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, math.abs(cam.transform.position.z)));
+                    Vector3 screenVector = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, math.abs(cam.transform.position.z))) - dragOffset;
             
                     rb.velocity = (screenVector - currentPos) / (Time.fixedDeltaTime * 4);
                 }
@@ -59,6 +64,7 @@ public class Draggable : MonoBehaviour {
             if (touch.phase == TouchPhase.Ended) {
                 dragging = false;
                 doDrag(false);
+                dragOffset = Vector3.zero;
             }
         }
     }
